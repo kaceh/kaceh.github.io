ce6da0604b63ebd5c6823830307dedecf1c35979
Kevin <kevin45christian@gmail.com>
2025-03-13 14:36:44 +0700

    Add a function to a button and group functions based on their respective pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,44 @@
-// Slider Function from: https://www.w3schools.com/howto/howto_js_slideshow.asp
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+// Homepage
+if (window.location.pathname === '/') {
+  // Slider Function from: https://www.w3schools.com/howto/howto_js_slideshow.asp
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+  
+  // End of Slider function
 }
 
-// End of Slider function
+// About page, button redirect
+if (window.location.pathname === '/about.html') {
+  const linkedinBtn = document.querySelector('.linkedinBtn');
+  if (linkedinBtn) {
+    linkedinBtn.addEventListener('click', function() {
+      window.open('https://www.linkedin.com/in/kevinc45/', '_blank');
+    });
+  }
+}
