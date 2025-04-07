4308cffc3b51b10c66b46209065b1896c6684ed7
Kevin <kevin45christian@gmail.com>
2025-04-07 13:28:14 +0700

    Add the sorting function for Projects page

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,59 @@ if (window.location.pathname === '/about.html') {
     });
   }
 }
+
+// Projects page, sorting function
+if (window.location.pathname === '/projects.html') {
+  const sort = document.getElementById('sort');
+  const container = document.getElementById('cardsContainer');
+
+  sort.addEventListener('change', (event) => {
+      const sortBy = event.target.value;
+      sortCards(sortBy);
+  });
+
+  function sortCards(sortBy) {
+
+      const cards = Array.from(container.querySelectorAll('.projectCard'));
+
+      let sortedCards;
+      switch (sortBy) {
+          case 'recent':
+              sortedCards = cards.sort((a, b) => {
+
+                  const dateA = new Date(a.dataset.date || 0);
+                  const dateB = new Date(b.dataset.date || 0);
+
+                  return dateB - dateA;
+              });
+              break;
+
+          case 'oldest':
+              sortedCards = cards.sort((a, b) => {
+
+                  const dateA = new Date(a.dataset.date || Infinity);
+                  const dateB = new Date(b.dataset.date || Infinity);
+
+                  return dateA - dateB;
+              });
+              break;
+
+          case 'alphabet':
+              sortedCards = cards.sort((a, b) => {
+
+                  const titleA = a.querySelector('.cardTitle')?.textContent.trim().toLowerCase() || '';
+                  const titleB = b.querySelector('.cardTitle')?.textContent.trim().toLowerCase() || '';
+
+                  return titleA.localeCompare(titleB);
+              });
+              break;
+
+          default:
+              sortedCards = cards;
+      }
+
+      sortedCards.forEach(card => {
+          container.appendChild(card);
+      });
+  }
+}
